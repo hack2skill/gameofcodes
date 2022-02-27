2b64873b51c81d50f8cc27c6200436a1847a21db
Karan <70339657+krn1904@users.noreply.github.com>
2022-02-27 16:25:48 +0530

    Update Karan_Soni_gameofcodes_Ps-1.java
    class name changed

--- Problem Statement-1/Solution/Karan_Soni_gamepfcodes_Ps-1/Karan_Soni_gameofcodes_Ps-1.java
+++ Problem Statement-1/Solution/Karan_Soni_gamepfcodes_Ps-1/Karan_Soni_gameofcodes_Ps-1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class h2c {
+public class h2s {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int test_case = sc.nextInt();
