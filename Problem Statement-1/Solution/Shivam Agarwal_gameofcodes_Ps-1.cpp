89d638cf9a583831212e0d6448da4af1d873dbc8
Shivam Agarwal <72163800+ShivamAgarwal-code@users.noreply.github.com>
2022-02-27 16:00:10 +0530

    Add files via upload
    Adding solution file for problem 1

---
+++ Problem Statement-1/Solution/Shivam Agarwal_gameofcodes_Ps-1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    // #endif
+    int t;
+    cin>>t;
+    while(t--){
+        int n,a,b,k,c=0;
+        for(int i=1;i<=n;i++){
+            if(i%a==0 && i%b!=0)
+                c++;
+            else if(i%a!=0 && i%b==0)
+                c++;
+        }
+        if(c>=k)
+            cout<<"Win"<<endl;
+        else
+            cout<<"Lose"<<endl;
+    }
+    return 0;
+}
