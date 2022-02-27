5cb3f848d529709fa5274f136ef4d14f70a8c039
dakshrepz <daksh.gupta@repozitory.com>
2022-02-27 15:42:44 +0530

    dakshgupta20024943_PS-1

---
+++ Problem Statement-1/Solution/dakshgupta20024943/dakshgupta20024943_gameofcode_PS-1.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    int t; cin>>t;
+    while (t--){
+
+        long long int n, a, b, k;
+        cin >> n >> a >> b >> k;
+
+        long long int problemSolved=0;
+
+        //divisible by a
+        problemSolved+=n/a;
+
+        //divisible by b
+        problemSolved+=n/b;
+
+        //divisible by a and b
+        problemSolved-=2*n/(a*b);
+
+        if (problemSolved>=k){
+            cout<<"Win"<<endl;
+        }
+        else{
+            cout<<"Lose"<<endl;
+        }
+    }
+
+}
